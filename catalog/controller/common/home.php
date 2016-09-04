e18d551a2570e0802629d7e836956e9f5b639e40
pearlscream <dimonich41@gmail.com>
2016-09-04 15:17:53 +0300

    Main page categories added

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -20,10 +20,32 @@ class ControllerCommonHome extends Controller {
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 
+
+		// TEST
+
+		$categories = $this->model_catalog_category->getCategories(0);
+		$data['categories'] = array();
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+				foreach ($children as $child) {
+					$child['description'] = str_replace('</p>',"",$child['description']);
+					$href = $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '&lines=0');
+					$child['href'] = $href;
+					array_push($data['categories'],$child);
+				}
+			}
+		}
+
+		 // TEST END
+
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/home.tpl')) {
 			$this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/common/home.tpl', $data));
 		} else {
 			$this->response->setOutput($this->load->view('default/template/common/home.tpl', $data));
 		}
+
+		$this->load->model('catalog/category');
 	}
 }
--- catalog/view/theme/service/template/common/home.tpl
+++ catalog/view/theme/service/template/common/home.tpl
@@ -1,8 +1,10 @@
 <?php echo $header; ?>
+
 <div class="container">
-  <?php mail('dimoni41@mail.ru', 'test', 'test','test')?>
   <div class="row"><?php echo $column_left; ?>
     <?php if ($column_left && $column_right) { ?>
+
+
     <?php $class = 'col-sm-6'; ?>
     <?php } elseif ($column_left || $column_right) { ?>
     <?php $class = 'col-sm-9'; ?>
@@ -12,6 +14,21 @@
     <div id="content" ><?php echo $content_top; ?><?php echo $content_bottom; ?></div>
     <?php echo $column_right; ?></div>
 </div>
+
+<?php foreach($categories as $category) { ?>
+<section class="white-and-grey-grid product-division">
+  <div class="wide-body-layout">
+    <div>
+      <h3 class="section-title"><a href="<?echo $category['href']?>"><?php echo $category['name']?></a></h3>
+      <img alt="" title="" src="image/<?php print_r($category[image]) ?>">
+      <p class="section-description"><?php echo $category['description']?></p>
+      <div class="button-wrapper">
+        <a href="<?echo $category['href']?>" class="blue-button">Подробнее</a>
+      </div>
+    </div>
+  </div>
+</section>
+<?php } ?>
 
 <script>
 </script>
