0fff53bda41f7c725fa7727caaebef1f3408d6ac
pearlscream <dimonich41@gmail.com>
2016-09-04 19:38:26 +0300

    Product count fixed

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -128,7 +128,7 @@ class ControllerCommonHeader extends Controller {
 						$href = $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'] . '&lines=0');
 					}
 					$children_data[] = array(
-						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'name'  => $child['name'],
 						'thumb'    => $image,
 						'href'  => $href
 					);
